797e5daba4e2c8210bbb1ffe2a12af69fbee2f20
Cheng Wang <wachmc@gmail.com>
2022-04-13 01:24:48 +0200

    Add webpack support

--- test/djb2.test.ts
+++ test/djb2.test.ts
@@ -16,6 +16,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
+import { Buffer } from 'buffer/'
 import djb2 from '../src/djb2'
 
 describe('djb2', function () {
