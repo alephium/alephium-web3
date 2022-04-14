4545a0be91c62ae878e1b4957a54a6df18aabec2
Cheng Wang <wachmc@gmail.com>
2022-04-14 18:42:34 +0200

    Use functional components for sample react project

--- templates/react/src/App.tsx
+++ templates/react/src/App.tsx
@@ -1,37 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 import { explorer, Contract, Script } from 'alephium-web3'
 import contractJson from './artifacts/greeter.ral.json'
 import scriptJson from './artifacts/greeter_main.ral.json'
 
-class Block extends React.Component<Record<string, never>, { blocks: string; contract: string; script: string }> {
-  readonly api = new explorer.Api<null>({ baseUrl: 'https://mainnet-backend.alephium.org' })
-  state = { blocks: '', contract: '', script: '' }
+const Dashboard = () => {
+  const api = new explorer.Api<null>({ baseUrl: 'https://mainnet-backend.alephium.org' })
+  const contract = Contract.fromJson(contractJson).toString()
+  const script = Script.fromJson(scriptJson).toString()
+  const [blocks, setBlocks] = useState('')
 
-  async componentDidMount() {
-    this.setState({ contract: Contract.fromJson(contractJson).toString() })
-    this.setState({ script: Script.fromJson(scriptJson).toString() })
-    const blocks = (await this.api.blocks.getBlocks({ page: 1 })).data.total
-    this.setState({ blocks: JSON.stringify(blocks) })
-  }
+  useEffect(() => {
+    async function fetchBlocks() {
+      const blocks = (await api.blocks.getBlocks({ page: 1 })).data.total
+      setBlocks(JSON.stringify(blocks))
+    }
 
-  render() {
-    return (
-      <div>
-        <p>blocks: {this.state.blocks}</p>
-        <p>contract: {this.state.contract}</p>
-        <p>script: {this.state.script}</p>
-      </div>
-    )
-  }
+    fetchBlocks()
+  })
+
+  return (
+    <div>
+      <p>blocks: {blocks}</p>
+      <p>contract: {contract}</p>
+      <p>script: {script}</p>
+    </div>
+  )
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Block />
+        <Dashboard />
       </header>
     </div>
   )
