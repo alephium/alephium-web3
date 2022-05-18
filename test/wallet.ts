6c9fe8b0b187cfe345b4ce6f2ee748948f75ed7c
Cheng Wang <wachmc@gmail.com>
2022-05-18 22:25:06 +0200

    Rename files

--- test/wallet.ts
+++ test/wallet.ts
@@ -17,7 +17,7 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { NodeProvider } from '../src/api'
-import { NodeWallet } from '../src/signer/NodeWallet'
+import { NodeWallet } from '../src/signer'
 
 export const testWalletName = 'alephium-web3-test-only-wallet'
 export const testAddress = '1DrDyTr9RpRsQnDnXo2YRiPzPW4ooHX5LLoqXrqfMrpQH'
