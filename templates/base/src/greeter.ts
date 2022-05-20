14da334f2d98fb5e6770b75898a78bf9d44d2e3b
Cheng Wang <wachmc@gmail.com>
2022-05-20 11:51:37 +0200

    Fix base

--- templates/base/src/greeter.ts
+++ templates/base/src/greeter.ts
@@ -2,7 +2,8 @@
  * greeter.ts
  */
 
-import { NodeProvider, Contract, Script, Signer, TestContractParams } from 'alephium-web3'
+import { NodeProvider, Contract, Script, TestContractParams } from 'alephium-web3'
+import { testWallet } from 'alephium-web3/test'
 
 async function greet() {
   const provider = new NodeProvider('http://127.0.0.1:22973')
@@ -10,24 +11,28 @@ async function greet() {
   const greeter = await Contract.fromSource(provider, 'greeter.ral')
 
   const testParams: TestContractParams = {
-    initialFields: [1]
+    initialFields: { btcPrice: 1 }
   }
   const testResult = await greeter.testPublicMethod(provider, 'greet', testParams)
   console.log(testResult)
 
-  const signer = Signer.testSigner(provider)
+  const signer = await testWallet(provider)
 
-  const deployTx = await greeter.transactionForDeployment(signer, testParams.initialFields)
+  const deployTx = await greeter.transactionForDeployment(signer, { initialFields: testParams.initialFields })
   const greeterContractId = deployTx.contractId
-  console.log(deployTx.group)
+  console.log(deployTx.fromGroup)
+  console.log(deployTx.toGroup)
 
   const submitResult = await signer.submitTransaction(deployTx.unsignedTx, deployTx.txId)
   console.log(submitResult)
 
   const main = await Script.fromSource(provider, 'greeter_main.ral')
 
-  const mainScriptTx = await main.transactionForDeployment(signer, { greeterContractId: greeterContractId })
-  console.log(mainScriptTx.group)
+  const mainScriptTx = await main.transactionForDeployment(signer, {
+    initialFields: { greeterContractId: greeterContractId }
+  })
+  console.log(mainScriptTx.fromGroup)
+  console.log(mainScriptTx.toGroup)
 
   const mainSubmitResult = await signer.submitTransaction(mainScriptTx.unsignedTx, mainScriptTx.txId)
   console.log(mainSubmitResult)
