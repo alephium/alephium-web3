c2bf840b66030939b5d5a84f22bf93e15b9756b8
Cheng Wang <wachmc@gmail.com>
2022-06-10 20:49:47 +0200

    Fix webpack merge

--- webpack.config.js
+++ webpack.config.js
@@ -16,28 +16,12 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-<<<<<<< HEAD
-const path = require('path')
 const webpack = require('webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-=======
-const webpack = require('webpack')
->>>>>>> alephium/master
 
 module.exports = {
   mode: 'production',
   entry: {
-<<<<<<< HEAD
     alephium: './dist/src/index.js'
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new webpack.SourceMapDevToolPlugin({ filename: '[file].map' }),
-    new webpack.IgnorePlugin({
-      checkResource(resource) {
-=======
-    alephium: './dist/lib/index.js'
   },
   plugins: [
     new webpack.SourceMapDevToolPlugin({ filename: '[file].map' }),
@@ -47,28 +31,10 @@ module.exports = {
     new webpack.IgnorePlugin({
       checkResource(resource) {
         // The "bip39/src/wordlists" node module consists of json files for multiple languages. We only need English.
->>>>>>> alephium/master
         return /.*\/wordlists\/(?!english).*\.json/.test(resource)
       }
     })
   ],
-<<<<<<< HEAD
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    fallback: {
-      fs: false,
-      stream: require.resolve('stream-browserify'),
-      crypto: require.resolve('crypto-browserify')
-=======
   resolve: {
     extensions: ['.js'],
     fallback: {
@@ -76,32 +42,10 @@ module.exports = {
       stream: require.resolve('stream-browserify'),
       crypto: require.resolve('crypto-browserify'),
       buffer: require.resolve('buffer/')
->>>>>>> alephium/master
     }
   },
   output: {
     filename: 'alephium-web3.min.js',
-<<<<<<< HEAD
-    path: path.resolve(__dirname, 'web'),
-    library: 'alephium-web3',
-    libraryTarget: 'umd'
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          format: {
-            comments: false
-          },
-          compress: true,
-          keep_classnames: true,
-          keep_fnames: true
-        },
-        extractComments: false
-      })
-    ]
-=======
     library: {
       name: 'alephium',
       type: 'umd'
@@ -109,6 +53,5 @@ module.exports = {
   },
   optimization: {
     minimize: true
->>>>>>> alephium/master
   }
 }
