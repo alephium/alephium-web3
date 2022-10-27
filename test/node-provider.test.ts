29a612f2327956139b525467ad22a24dd60b26b4
Cheng Wang <wachmc@gmail.com>
2022-10-27 12:09:57 +0200

    Fix provider proxy

--- test/node-provider.test.ts
+++ test/node-provider.test.ts
@@ -19,14 +19,30 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 import { testAddress, testWalletName } from '@alephium/web3-test'
 import { web3, NodeProvider } from '../packages/web3/src'
 
-describe('remote node provider', () => {
-  it('forward requests', async () => {
-    web3.setCurrentNodeProvider('http://127.0.0.1:22973')
-    const nodeProvider = web3.getCurrentNodeProvider()
+describe('node provider', () => {
+  web3.setCurrentNodeProvider('http://127.0.0.1:22973')
+  const nodeProvider = web3.getCurrentNodeProvider()
+
+  it('remote node provider should forward requests', async () => {
     const request = nodeProvider.request
     const remoteNodeProvider = NodeProvider.Remote(request)
     console.log(await remoteNodeProvider.wallets.getWallets())
     console.log(await remoteNodeProvider.wallets.getWalletsWalletNameAddressesAddress(testWalletName, testAddress))
     console.log(await remoteNodeProvider.infos.getInfosVersion())
+  })
+
+  it('proxied node provider should clone the source provider', () => {
+    const sourceFunc0 = nodeProvider.wallets.getWallets.toString()
+    const proxy = NodeProvider.Proxy(nodeProvider)
+    const proxyFunc0 = proxy.wallets.getWallets.toString()
+    expect(proxyFunc0).toEqual(sourceFunc0)
+    proxy.wallets.getWallets = () => Promise.reject('')
+    const sourceFunc1 = nodeProvider.wallets.getWallets.toString()
+    const proxyFunc1 = proxy.wallets.getWallets.toString()
+    expect(sourceFunc1).toEqual(sourceFunc0)
+    expect(sourceFunc1).not.toEqual(`() => Promise.reject('')`)
+    expect(proxyFunc1).not.toEqual(proxyFunc0)
+    expect(proxyFunc1).not.toEqual(sourceFunc1)
+    expect(proxyFunc1).toEqual(`() => Promise.reject('')`)
   })
 })
