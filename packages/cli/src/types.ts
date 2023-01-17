b9a61ccc0d050ba272b85b676b813b22f0e06595
Cheng Wang <wachmc@gmail.com>
2023-01-17 16:14:55 +0100

    Update node to 1.6.1

--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -58,7 +58,7 @@ export interface Configuration<Settings = unknown> {
 }
 
 export const DEFAULT_CONFIGURATION_VALUES = {
-  nodeVersion: '1.6.0-rc5',
+  nodeVersion: '1.6.1',
   nodeConfigFile: 'devnet-user.conf',
   toDeployGroups: [0],
   sourceDir: Project.DEFAULT_CONTRACTS_DIR,
