5373823a813dc93194078373ea195ee6e94f83ad
lbqds <lbqds@outlook.com>
2023-02-22 11:26:44 +0800

    Fix private key to hex string

--- packages/web3/src/utils/sign.test.ts
+++ packages/web3/src/utils/sign.test.ts
@@ -36,7 +36,7 @@ describe('Signing', function () {
   it('should sign and verify schnorr signature', () => {
     const ec = new EC.ec('secp256k1')
     const key = ec.genKeyPair()
-    const privateKey = key.getPrivate().toString('hex')
+    const privateKey = key.getPrivate().toString('hex', 64)
     const publicKey = publicKeyFromPrivateKey(privateKey, 'bip340-schnorr')
 
     const hash = '8fc5f0d120b730f97f6cea5f02ae4a6ee7bf451d9261c623ea69d85e870201d2'
