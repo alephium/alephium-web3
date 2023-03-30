0ab6c69ad9ef5f8a47c0efa0a0603bbdac076fd2
Ilias Trichopoulos <hlias.nop@gmail.com>
2023-03-30 15:59:02 +0200

    Add file headers

--- packages/web3-react/rollup.config.prod.js
+++ packages/web3-react/rollup.config.prod.js
@@ -1,7 +1,24 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import typescript from 'rollup-plugin-typescript2';
+/*
+Copyright 2018 - 2022 The Alephium Authors
+This file is part of the alephium project.
 
-import packageJson from './package.json';
+The library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the library. If not, see <http://www.gnu.org/licenses/>.
+*/
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import typescript from 'rollup-plugin-typescript2'
+
+import packageJson from './package.json'
 
 export default [
   {
