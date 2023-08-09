0cde73ab6367ebec3e7311aae4f101c3f16b4a63
lbqds <lbqds@outlook.com>
2023-08-09 20:11:43 +0800

    Remove balances from profile modal

--- packages/web3-react/src/components/Pages/Profile/index.tsx
+++ packages/web3-react/src/components/Pages/Profile/index.tsx
@@ -18,23 +18,18 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 import React, { useEffect, useState } from 'react'
 import { useConnectSettingContext } from '../../../contexts/alephiumConnect'
 
-import { PageContent, ModalBody, ModalContent, ModalH1 } from '../../Common/Modal/styles'
+import { PageContent, ModalContent, ModalH1 } from '../../Common/Modal/styles'
 import Button from '../../Common/Button'
 
 import { DisconnectIcon } from '../../../assets/icons'
 import CopyToClipboard from '../../Common/CopyToClipboard'
 import { useAccount } from '../../../hooks/useAccount'
 import { truncatedAddress } from '../../../utils'
-import { useBalance } from '../../../hooks/useBalance'
-import { AnimatePresence } from 'framer-motion'
-import { Balance, BalanceContainer, LoadingBalance } from './styles'
-import { prettifyAttoAlphAmount } from '@alephium/web3'
 import { useConnect } from '../../../hooks/useConnect'
 
 const Profile: React.FC<{ closeModal?: () => void }> = ({ closeModal }) => {
   const context = useConnectSettingContext()
   const account = useAccount()
-  const { balance } = useBalance()
   const { disconnect } = useConnect({
     addressGroup: context.addressGroup,
     networkId: context.network
@@ -62,33 +57,6 @@ const Profile: React.FC<{ closeModal?: () => void }> = ({ closeModal }) => {
         <ModalH1>
           <CopyToClipboard string={address}>{address && truncatedAddress(address)}</CopyToClipboard>
         </ModalH1>
-        <ModalBody>
-          <BalanceContainer>
-            <AnimatePresence exitBeforeEnter initial={false}>
-              {balance && (
-                <Balance
-                  key={`alephium`}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.2 }}
-                >
-                  {prettifyAttoAlphAmount(BigInt(balance.balance))} ALPH
-                </Balance>
-              )}
-              {!balance && (
-                <LoadingBalance
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.2 }}
-                >
-                  &nbsp;
-                </LoadingBalance>
-              )}
-            </AnimatePresence>
-          </BalanceContainer>
-        </ModalBody>
       </ModalContent>
       <Button onClick={() => setShouldDisconnect(true)} icon={<DisconnectIcon />}>
         {'Disconnect'}
