18892efbabcf4198d17683d216f4a55509f89fd4
lbqds <lbqds@outlook.com>
2024-02-27 21:42:35 +0800

    Fix lint and tests

--- packages/web3-test/src/test-wallet.ts
+++ packages/web3-test/src/test-wallet.ts
@@ -133,7 +133,9 @@ export async function transfer(from: PrivateKeyWallet, to: Address, tokenId: str
 
 export async function expectAssertionError(p: Promise<unknown>, address: string, errorCode: number): Promise<void> {
   expect(isBase58(address)).toEqual(true)
-  await expect(p).rejects.toThrowError(new RegExp(`VM execution error: Assertion Failed in Contract @ ${address}, Error Code: ${errorCode}`, 'mg'))
+  await expect(p).rejects.toThrowError(
+    new RegExp(`VM execution error: Assertion Failed in Contract @ ${address}, Error Code: ${errorCode}`, 'mg')
+  )
 }
 
 export function randomContractId(): string {
--- packages/web3/src/contract/index.ts
+++ packages/web3/src/contract/index.ts
@@ -16,6 +16,6 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-export { buildScriptByteCode, buildContractByteCode } from './ralph'
+export * from './ralph'
 export * from './contract'
 export * from './events'
