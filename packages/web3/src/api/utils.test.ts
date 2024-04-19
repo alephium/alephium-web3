c9ef79f54a8fe4cb66461790e376e368d3a0d6aa
h0ngcha0 <lbs.lhc@gmail.com>
2024-04-19 14:36:55 +0200

    Remove unused imports

--- packages/web3/src/api/utils.test.ts
+++ packages/web3/src/api/utils.test.ts
@@ -16,7 +16,6 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { enableDebugMode, groupOfAddress } from '..'
 import { ONE_ALPH } from '../constants'
 import { NodeProvider, isBalanceEqual, node } from './index'
 
