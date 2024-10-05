428690001faa2d4fe9fbe0e1fff3291937f9cd0d
h0ngcha0 <lbs.lhc@gmail.com>
2024-10-05 15:53:36 +0200

    Upgrade full node version to 2.8.1

--- packages/web3/src/api/api-alephium.ts
+++ packages/web3/src/api/api-alephium.ts
@@ -1650,7 +1650,7 @@ export class HttpClient<SecurityDataType = unknown> {
 
 /**
  * @title Alephium API
- * @version 3.7.1
+ * @version 3.8.1
  * @baseUrl ../
  */
 export class Api<SecurityDataType extends unknown> extends HttpClient<SecurityDataType> {
