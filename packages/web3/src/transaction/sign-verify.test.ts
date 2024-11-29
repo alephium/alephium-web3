10abe616b671b8afdb1ec2fee855b071ce10a5eb
Collins C Augustine <colins4chinonso@gmail.com>
2024-11-29 18:38:14 +0100

    fix: implement requested chamges to sign-verify.test.ts file

--- packages/web3/src/transaction/sign-verify.test.ts
+++ packages/web3/src/transaction/sign-verify.test.ts
@@ -16,147 +16,66 @@ You should have received a copy of the GNU Lesser General Public License
 along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-import EC from 'elliptic'
+import { PrivateKeyWallet } from '@alephium/web3-wallet'
 import { transactionSign, transactionVerifySignature } from './sign-verify'
 import { KeyType } from '../signer'
+import crypto from 'crypto'
+
+// Helper function to generate random transaction IDs for testing
+function generateRandomTxId(): string {
+    return crypto.randomBytes(32).toString('hex')
+}
+
 
 describe('transaction', function () {
+  // Basic functionality tests
   it('should verify signature', () => {
-    const txHash = '8fc5f0d120b730f97f6cea5f02ae4a6ee7bf451d9261c623ea69d85e870201d2'
-    const pubKey = '02625b26ae1c5f7986475009e4037b3e6fe6320fde3c3f3332bea11ecadc35dd13'
-    const txSig =
-      '78471e7c97e558c98ac307ef699ed535ece319102fc69ea416dbb44fbb3cbf9c42dbfbf4ce73eb68c5e0d66122eb25d2ebe1cf9e37ef4c4f4e7a2ed35de141bc'
-    const unnormalizedSig =
-      '78471e7c97e558c98ac307ef699ed535ece319102fc69ea416dbb44fbb3cbf9cbd24040b318c14973a1f299edd14da2bcecd0d48775953ec71582fb97254ff85'
-    const wrongSig =
-      '88471e7c97e558c98ac307ef699ed535ece319102fc69ea416dbb44fbb3cbf9c42dbfbf4ce73eb68c5e0d66122eb25d2ebe1cf9e37ef4c4f4e7a2ed35de141bc'
+      const testWallet = PrivateKeyWallet.Random()
+      const testTxHash = generateRandomTxId()
+      const validSignature = transactionSign(testTxHash, testWallet.privateKey)
+      
+      // Test normal verification
+      expect(transactionVerifySignature(testTxHash, testWallet.publicKey, validSignature)).toEqual(true)
 
-    expect(transactionVerifySignature(txHash, pubKey, txSig)).toEqual(true)
-    expect(transactionVerifySignature(txHash, pubKey, unnormalizedSig)).toEqual(false)
-    expect(transactionVerifySignature(txHash, pubKey, wrongSig)).toEqual(false)
+      // Test with modified signatures
+      const unnormalizedSig = validSignature.slice(0, -2) + 'ff'
+      const wrongSig = '1' + validSignature.slice(1)
+      expect(transactionVerifySignature(testTxHash, testWallet.publicKey, unnormalizedSig)).toEqual(false)
+      expect(transactionVerifySignature(testTxHash, testWallet.publicKey, wrongSig)).toEqual(false)
   })
 
   it('should sign and verify signature', () => {
-    const ec = new EC.ec('secp256k1')
-    const key = ec.genKeyPair()
-    const privateKey = key.getPrivate().toString('hex')
-    const publicKey = key.getPublic().encode('hex', true)
-
-    const txHash = '8fc5f0d120b730f97f6cea5f02ae4a6ee7bf451d9261c623ea69d85e870201d2'
-    const signature = transactionSign(txHash, privateKey)
-    expect(transactionVerifySignature(txHash, publicKey, signature)).toEqual(true)
-  })
-
-  describe('transactionVerifySignature additional tests', () => {
-    const validTxHash = '8fc5f0d120b730f97f6cea5f02ae4a6ee7bf451d9261c623ea69d85e870201d2'
-    const validPubKey = '02625b26ae1c5f7986475009e4037b3e6fe6320fde3c3f3332bea11ecadc35dd13'
-    const validSignature = '78471e7c97e558c98ac307ef699ed535ece319102fc69ea416dbb44fbb3cbf9c42dbfbf4ce73eb68c5e0d66122eb25d2ebe1cf9e37ef4c4f4e7a2ed35de141bc'
-
-    it('should handle different key types', () => {
-      const ec = new EC.ec('secp256k1')
-      const key = ec.genKeyPair()
-      const privateKey = key.getPrivate().toString('hex')
-      const publicKey = key.getPublic().encode('hex', true)
-      const txHash = validTxHash
-
-      const signature = transactionSign(txHash, privateKey)
-      expect(transactionVerifySignature(txHash, publicKey, signature)).toEqual(true)
-    })
-
-    it('should handle invalid public keys', () => {
-      expect(transactionVerifySignature(validTxHash, 'invalid-pubkey', validSignature)).toEqual(false)
-      expect(transactionVerifySignature(validTxHash, '', validSignature)).toEqual(false)
-      expect(transactionVerifySignature(validTxHash, '0123456789', validSignature)).toEqual(false)
-    })
-
-    it('should handle invalid transaction hashes', () => {
-      expect(transactionVerifySignature('invalid-hash', validPubKey, validSignature)).toEqual(false)
-      expect(transactionVerifySignature('', validPubKey, validSignature)).toEqual(false)
-      expect(transactionVerifySignature('0123456789', validPubKey, validSignature)).toEqual(false)
-    })
-
-    it('should handle invalid signatures', () => {
-      expect(transactionVerifySignature(validTxHash, validPubKey, 'invalid-sig')).toEqual(false)
-      expect(transactionVerifySignature(validTxHash, validPubKey, '')).toEqual(false)
-      expect(transactionVerifySignature(validTxHash, validPubKey, '0123456789')).toEqual(false)
-    })
-  })
-
-})
-
-describe('transactionSign additional tests', () => {
-  const validTxHash = '8fc5f0d120b730f97f6cea5f02ae4a6ee7bf451d9261c623ea69d85e870201d2'
-  const validPubKey = '02625b26ae1c5f7986475009e4037b3e6fe6320fde3c3f3332bea11ecadc35dd13'
-
-  it('should handle invalid private keys', () => {
-    // Test with invalid private key formats
-    const invalidPrivateKeys = ['invalid-key', '', '0123456789', 'not-hex', '@#$%^&*']
-    
-    invalidPrivateKeys.forEach(invalidKey => {
-      // Since invalid private keys might throw errors, we wrap in try-catch
-      try {
-        const signature = transactionSign(validTxHash, invalidKey)
-        // If no error thrown, the signature should fail verification
-        expect(transactionVerifySignature(validTxHash, validPubKey, signature)).toBeFalsy()
-      } catch (error) {
-        // Error thrown is also an acceptable outcome
-        expect(error).toBeTruthy()
-      }
-    })
-  })
-
-  it('should handle invalid transaction hashes', () => {
-    // Generate a valid private key for testing
-    const ec = new EC.ec('secp256k1')
-    const keyPair = ec.genKeyPair()
-    const validPrivateKey = keyPair.getPrivate().toString('hex')
-    
-    // Test with invalid transaction hash formats
-    const invalidHashes = ['invalid-hash', '', '0123456789', 'not-hex', '@#$%^&*']
-    
-    invalidHashes.forEach(invalidHash => {
-      const signature = transactionSign(invalidHash, validPrivateKey)
-      // Either the signature should be invalid when verified, or signing should return an empty/invalid signature
-      if (signature) {
-        expect(transactionVerifySignature(invalidHash, validPubKey, signature)).toBeFalsy()
-      } else {
-        expect(signature).toBeFalsy()
-      }
-    })
-  })
-
-  it('should handle various combinations of invalid inputs', () => {
-    // Test with combinations of invalid inputs
-    const invalidInputs = ['invalid-input', '', '0123456789']
-    
-    invalidInputs.forEach(invalidInput => {
-      // Test invalid hash with valid private key
-      const ec = new EC.ec('secp256k1')
-      const keyPair = ec.genKeyPair()
-      const validPrivateKey = keyPair.getPrivate().toString('hex')
+      const testWallet = PrivateKeyWallet.Random()
+      const testTxHash = generateRandomTxId()
+      const testSignature = transactionSign(testTxHash, testWallet.privateKey)
       
-      try {
-        const sig1 = transactionSign(invalidInput, validPrivateKey)
-        expect(transactionVerifySignature(invalidInput, validPubKey, sig1)).toBeFalsy()
-      } catch (error) {
-        expect(error).toBeTruthy()
-      }
+      expect(transactionVerifySignature(testTxHash, testWallet.publicKey, testSignature)).toEqual(true)
+  })
 
-      // Test valid hash with invalid private key
-      try {
-        const sig2 = transactionSign(validTxHash, invalidInput)
-        expect(transactionVerifySignature(validTxHash, validPubKey, sig2)).toBeFalsy()
-      } catch (error) {
-        expect(error).toBeTruthy()
-      }
+  describe('transactionVerifySignature validation', () => {
+      const testWallet = PrivateKeyWallet.Random()
+      const validTxHash = generateRandomTxId()
+      const validSignature = transactionSign(validTxHash, testWallet.privateKey)
 
-      // Test both invalid hash and invalid private key
-      try {
-        const sig3 = transactionSign(invalidInput, invalidInput)
-        expect(transactionVerifySignature(invalidInput, validPubKey, sig3)).toBeFalsy()
-      } catch (error) {
-        expect(error).toBeTruthy()
-      }
+      it('should handle invalid public keys', () => {
+          const invalidKeys = ['invalid-pubkey', '', '0123456789']
+          invalidKeys.forEach(invalidKey => {
+              expect(transactionVerifySignature(validTxHash, invalidKey, validSignature)).toEqual(false)
+          })
+      })
+
+      it('should handle invalid transaction hashes', () => {
+        const invalidHashes = ['invalid-hash', '', '0123456789']
+        invalidHashes.forEach(invalidHash => {
+            expect(transactionVerifySignature(invalidHash, testWallet.publicKey, validSignature)).toEqual(false)
+          })
     })
+
+      it('should handle invalid signatures', () => {
+          const invalidSignatures = ['invalid-sig', '', '0123456789']
+          invalidSignatures.forEach(invalidSig => {
+              expect(transactionVerifySignature(validTxHash, testWallet.publicKey, invalidSig)).toEqual(false)
+          })
+      })
   })
 })
--- packages/web3/src/transaction/utils.test.ts
+++ packages/web3/src/transaction/utils.test.ts
@@ -17,7 +17,7 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { getSigner, getSigners } from '@alephium/web3-test'
-import { groupIndexOfTransaction, waitForTxConfirmation } from './utils'
+import { groupIndexOfTransaction } from './utils'
 import { ONE_ALPH, TOTAL_NUMBER_OF_GROUPS } from '../constants'
 import { bs58, hexToBinUnsafe } from '../utils'
 import { unsignedTxCodec } from '../codec'
@@ -25,287 +25,107 @@ import { groupOfAddress } from '../address'
 import { blake2b } from 'blakejs'
 import { web3 } from '../'
 import { PrivateKeyWallet } from '@alephium/web3-wallet'
-import { node } from '../api'
 
 describe('transaction utils', () => {
   beforeAll(() => {
     web3.setCurrentNodeProvider('http://127.0.0.1:22973', undefined, fetch)
   })
 
-  describe('transaction confirmation', () => {
-    let originalProvider: any
+  it('should calculate the group of transfer transaction', async () => {
+    const signer1 = await getSigner(undefined, randomGroup())
+    const signer2 = await getSigner(undefined, randomGroup())
+    const fromAccount = await signer1.getSelectedAccount()
+    const toAccount = await signer2.getSelectedAccount()
 
-    beforeEach(() => {
-      originalProvider = web3.getCurrentNodeProvider()
+    const tx = await signer1.buildTransferTx({
+      signerAddress: fromAccount.address,
+      destinations: [{ address: toAccount.address, attoAlphAmount: ONE_ALPH }]
     })
 
-    afterEach(() => {
-      web3.setCurrentNodeProvider(originalProvider)
-    })
+    const unsignedTx = unsignedTxCodec.decode(hexToBinUnsafe(tx.unsignedTx))
+    const [fromGroup, toGroup] = groupIndexOfTransaction(unsignedTx)
 
-    it('should wait for transaction confirmation', async () => {
-      const mockConfirmed: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 2,
-        fromGroupConfirmations: 2,
-        toGroupConfirmations: 2
-      }
-
-      const mockProvider = {
-        transactions: {
-          getTransactionsStatus: jest.fn().mockResolvedValue(mockConfirmed)
-        }
-      }
-      web3.setCurrentNodeProvider(mockProvider as any)
-
-      const result = await waitForTxConfirmation('test-tx-id', 1, 100)
-      expect(result.type).toBe('Confirmed')
-      expect(result.chainConfirmations).toBe(2)
-    })
-
-    it('should poll until transaction is confirmed', async () => {
-      const mockMempool: node.MemPooled = { type: 'MemPooled' }
-      const mockConfirmed: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 1,
-        fromGroupConfirmations: 1,
-        toGroupConfirmations: 1
-      }
-
-      const mockProvider = {
-        transactions: {
-          getTransactionsStatus: jest.fn()
-            .mockResolvedValueOnce(mockMempool)
-            .mockResolvedValueOnce(mockConfirmed)
-        }
-      }
-      web3.setCurrentNodeProvider(mockProvider as any)
-
-      const result = await waitForTxConfirmation('test-tx-id', 1, 10)
-      expect(result.type).toBe('Confirmed')
-      expect(mockProvider.transactions.getTransactionsStatus).toHaveBeenCalledTimes(2)
-    })
-
-    it('should handle transaction errors', async () => {
-      const mockProvider = {
-        transactions: {
-          getTransactionsStatus: jest.fn().mockRejectedValue(new Error('Transaction not found'))
-        }
-      }
-      web3.setCurrentNodeProvider(mockProvider as any)
-
-      await expect(waitForTxConfirmation('invalid-tx', 1, 100)).rejects.toThrow('Transaction not found')
-    })
-
-    it('should handle multiple confirmation levels', async () => {
-      const mockSequence = [
-        { type: 'MemPooled' } as node.MemPooled,
-        {
-          type: 'Confirmed',
-          blockHash: '1234',
-          txIndex: 0,
-          chainConfirmations: 1,
-          fromGroupConfirmations: 1,
-          toGroupConfirmations: 1
-        } as node.Confirmed,
-        {
-          type: 'Confirmed',
-          blockHash: '1234',
-          txIndex: 0,
-          chainConfirmations: 3,
-          fromGroupConfirmations: 3,
-          toGroupConfirmations: 3
-        } as node.Confirmed
-      ]
-
-      const mockProvider = {
-        transactions: {
-          getTransactionsStatus: jest.fn()
-            .mockResolvedValueOnce(mockSequence[0])
-            .mockResolvedValueOnce(mockSequence[1])
-            .mockResolvedValueOnce(mockSequence[2])
-        }
-      }
-      web3.setCurrentNodeProvider(mockProvider as any)
-
-      const result = await waitForTxConfirmation('test-tx-id', 3, 10)
-      expect(result.type).toBe('Confirmed')
-      expect(result.chainConfirmations).toBe(3)
-      expect(mockProvider.transactions.getTransactionsStatus).toHaveBeenCalledTimes(3)
-    })
-
-    it('should handle network errors during polling', async () => {
-      const mockConfirmed: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 1,
-        fromGroupConfirmations: 1,
-        toGroupConfirmations: 1
-      }
-  
-      const mockProvider = {
-        transactions: {
-          getTransactionsStatus: jest.fn()
-            .mockRejectedValueOnce(new Error('Network error'))
-            .mockResolvedValueOnce(mockConfirmed)
-            .mockResolvedValueOnce(mockConfirmed) 
-        }
-      }
-      web3.setCurrentNodeProvider(mockProvider as any)
-  
-      try {
-        await waitForTxConfirmation('test-tx-id', 1, 10)
-      } catch (error) {
-        const result = await waitForTxConfirmation('test-tx-id', 1, 10)
-        expect(result.type).toBe('Confirmed')
-        expect(mockProvider.transactions.getTransactionsStatus).toHaveBeenCalled()
-      }
-    })      
-
+    expect(fromGroup).toEqual(groupOfAddress(fromAccount.address))
+    expect(toGroup).toEqual(groupOfAddress(toAccount.address))
   })
 
-  describe('transaction types', () => {
-    it('should identify confirmed transactions', () => {
-      const confirmedTx: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 1,
-        fromGroupConfirmations: 1,
-        toGroupConfirmations: 1
-      }
+  it('should calculate the group of multisig transaction', async () => {
+    const nodeProvider = web3.getCurrentNodeProvider()
+    const [signer1, signer2, signer3] = await getSigners(3, undefined, randomGroup())
+    const pkh1 = toPublicKeyHash(signer1.publicKey)
+    const pkh2 = toPublicKeyHash(signer2.publicKey)
+    const pkh3 = toPublicKeyHash(signer3.publicKey)
+    const multisigAddress = bs58.encode(new Uint8Array([0x01, 0x03, ...pkh1, ...pkh2, ...pkh3, 0x02]))
+    const fromAccount = await signer1.getSelectedAccount()
 
-      const confirmedTxHighConfirm: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '5678',
-        txIndex: 1,
-        chainConfirmations: 10,
-        fromGroupConfirmations: 8,
-        toGroupConfirmations: 9
-      }
-
-      expect(confirmedTx.type).toBe('Confirmed')
-      expect(confirmedTxHighConfirm.type).toBe('Confirmed')
-      expect(confirmedTx.chainConfirmations).toBeLessThan(confirmedTxHighConfirm.chainConfirmations)
-    })
-
-    it('should identify non-confirmed transactions', () => {
-      const mempoolTx: node.MemPooled = {
-        type: 'MemPooled'
-      }
-      expect(mempoolTx.type === 'Confirmed').toBe(false)
-    })
-
-    it('should identify mempool transactions', () => {
-      const mempoolTx: node.MemPooled = { type: 'MemPooled' }
-      expect(mempoolTx.type).toBe('MemPooled')
-      expect(mempoolTx.type).not.toBe('Confirmed')
-
-      const mempoolTxs: node.MemPooled[] = [
-        { type: 'MemPooled' },
-        { type: 'MemPooled' }
-      ]
-      mempoolTxs.forEach(tx => {
-        expect(tx.type).toBe('MemPooled')
+    {
+      // Transfer to multisig address
+      const toMultiSig = await signer1.signAndSubmitTransferTx({
+        signerAddress: fromAccount.address,
+        destinations: [{ address: multisigAddress, attoAlphAmount: ONE_ALPH * 10n }]
       })
-    })
-
-    it('should handle transition from mempool to confirmed', () => {
-      const mempoolTx: node.MemPooled = { type: 'MemPooled' }
-      const confirmedTx1: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 1,
-        fromGroupConfirmations: 1,
-        toGroupConfirmations: 1
-      }
-      const confirmedTx2: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 2,
-        fromGroupConfirmations: 2,
-        toGroupConfirmations: 2
-      }
-
-      expect(mempoolTx.type).toBe('MemPooled')
-      expect(confirmedTx1.type).toBe('Confirmed')
-      expect(confirmedTx2.type).toBe('Confirmed')
-      expect(confirmedTx2.chainConfirmations).toBeGreaterThan(confirmedTx1.chainConfirmations)
-    })
-
-    it('should validate confirmed transaction properties', () => {
-      const confirmedTx: node.Confirmed = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 5,
-        fromGroupConfirmations: 4,
-        toGroupConfirmations: 4
-      }
-
-      expect(confirmedTx.type).toBe('Confirmed')
-      expect(confirmedTx.blockHash).toBeTruthy()
-      expect(typeof confirmedTx.txIndex).toBe('number')
-      expect(confirmedTx.chainConfirmations).toBeGreaterThan(0)
-      expect(confirmedTx.fromGroupConfirmations).toBeLessThanOrEqual(confirmedTx.chainConfirmations)
-      expect(confirmedTx.toGroupConfirmations).toBeLessThanOrEqual(confirmedTx.chainConfirmations)
-    })
-
-    it('should identify transaction types correctly', () => {
-      const confirmedTx: node.TxStatus = {
-        type: 'Confirmed',
-        blockHash: '1234',
-        txIndex: 0,
-        chainConfirmations: 1,
-        fromGroupConfirmations: 1,
-        toGroupConfirmations: 1
-      }
-      const mempoolTx: node.TxStatus = { type: 'MemPooled' }
-
-      expect(confirmedTx.type).toBe('Confirmed')
-      expect(mempoolTx.type).toBe('MemPooled')
-    })
-  })
-
-describe('transaction type guard tests', () => {
-  it('should correctly identify different transaction statuses', () => {
-    const confirmedTx: node.TxStatus = {
-      type: 'Confirmed',
-      blockHash: '1234',
-      txIndex: 0,
-      chainConfirmations: 1,
-      fromGroupConfirmations: 1,
-      toGroupConfirmations: 1
-    }
-    
-    const mempoolTx: node.TxStatus = {
-      type: 'MemPooled'
+      const unsignedTx = unsignedTxCodec.decode(hexToBinUnsafe(toMultiSig.unsignedTx))
+      const [fromGroup, toGroup] = groupIndexOfTransaction(unsignedTx)
+      expect(fromGroup).toEqual(groupOfAddress(fromAccount.address))
+      expect(toGroup).toEqual(groupOfAddress(multisigAddress))
     }
 
-    if (confirmedTx.type === 'Confirmed') {
-      expect(confirmedTx.chainConfirmations).toBeDefined()
-      expect(confirmedTx.blockHash).toBeDefined()
-    }
+    {
+      // Transfer from multisig address
+      const fromMultiSig = await nodeProvider.multisig.postMultisigBuild({
+        fromAddress: multisigAddress,
+        fromPublicKeys: [signer1.publicKey, signer2.publicKey],
+        destinations: [{ address: signer1.address, attoAlphAmount: ONE_ALPH.toString() }]
+      })
+      const unsignedTx = unsignedTxCodec.decode(hexToBinUnsafe(fromMultiSig.unsignedTx))
+      const [fromGroup, toGroup] = groupIndexOfTransaction(unsignedTx)
 
-    if (mempoolTx.type === 'MemPooled') {
-      expect(Object.keys(mempoolTx).length).toBe(1)
+      expect(fromGroup).toEqual(groupOfAddress(multisigAddress))
+      expect(toGroup).toEqual(groupOfAddress(signer1.address))
     }
   })
-})
 
-  // Helper functions
-  function randomGroup(): number {
-    return Math.floor(Math.random() * TOTAL_NUMBER_OF_GROUPS)
-  }
+  it('should calculate the group of p2sh transaction', async () => {
+    const nodeProvider = web3.getCurrentNodeProvider()
+    const signer1 = await getSigner(undefined, randomGroup())
+    const schnorrSigner = PrivateKeyWallet.Random(randomGroup(), nodeProvider, 'bip340-schnorr')
+    const fromAccount = await signer1.getSelectedAccount()
+
+    {
+      // Transfer to schnorr address
+      const toSchnorrAddressResult = await signer1.signAndSubmitTransferTx({
+        signerAddress: fromAccount.address,
+        destinations: [{ address: schnorrSigner.address, attoAlphAmount: ONE_ALPH }]
+      })
+
+      const unsignedTx = unsignedTxCodec.decode(hexToBinUnsafe(toSchnorrAddressResult.unsignedTx))
+      const [fromGroup, toGroup] = groupIndexOfTransaction(unsignedTx)
+
+      expect(fromGroup).toEqual(groupOfAddress(fromAccount.address))
+      expect(toGroup).toEqual(groupOfAddress(schnorrSigner.address))
+    }
+
+    {
+      // Transfer from schnorr address
+      const fromSchnorrAddressResult = await schnorrSigner.signAndSubmitTransferTx({
+        signerAddress: schnorrSigner.address,
+        signerKeyType: 'bip340-schnorr',
+        destinations: [{ address: fromAccount.address, attoAlphAmount: ONE_ALPH / 2n }]
+      })
+
+      const unsignedTx = unsignedTxCodec.decode(hexToBinUnsafe(fromSchnorrAddressResult.unsignedTx))
+      const [fromGroup, toGroup] = groupIndexOfTransaction(unsignedTx)
+
+      expect(fromGroup).toEqual(groupOfAddress(schnorrSigner.address))
+      expect(toGroup).toEqual(groupOfAddress(fromAccount.address))
+    }
+  })
 
   function toPublicKeyHash(publicKey: string): Uint8Array {
     return blake2b(hexToBinUnsafe(publicKey), undefined, 32)
+  }
+
+  function randomGroup(): number {
+    return Math.floor(Math.random() * TOTAL_NUMBER_OF_GROUPS)
   }
 })
