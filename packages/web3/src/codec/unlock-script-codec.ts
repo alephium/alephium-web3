fb52650c7e6c00b7f12e46cbb8ab7bd6059e7b45
lbqds <lbqds@outlook.com>
2025-05-29 10:35:54 +0800

    Add p2hmpk codec

--- packages/web3/src/codec/unlock-script-codec.ts
+++ packages/web3/src/codec/unlock-script-codec.ts
@@ -17,9 +17,10 @@ along with the library. If not, see <http://www.gnu.org/licenses/>.
 */
 import { ArrayCodec } from './array-codec'
 import { i32Codec } from './compact-int-codec'
-import { byteCodec, Codec, EnumCodec, FixedSizeCodec, ObjectCodec } from './codec'
+import { Codec, EnumCodec, FixedSizeCodec, ObjectCodec } from './codec'
 import { Script, scriptCodec } from './script-codec'
 import { Val, valsCodec } from './val'
+import { PublicKeyLike, publicKeyLikeCodec } from './public-key-like-codec'
 
 export type PublicKey = Uint8Array
 export interface KeyWithIndex {
@@ -34,7 +35,10 @@ export interface P2SH {
 export type SameAsPrevious = 'SameAsPrevious'
 export type P2PK = 'P2PK'
 
-export type KeyType = number
+export interface P2HMPK {
+  publicKeys: PublicKeyLike[]
+  publicKeyIndexes: number[]
+}
 
 export type UnlockScript =
   | { kind: 'P2PKH'; value: PublicKey }
@@ -43,6 +47,7 @@ export type UnlockScript =
   | { kind: 'SameAsPrevious'; value: SameAsPrevious }
   | { kind: 'PoLW'; value: PublicKey }
   | { kind: 'P2PK'; value: P2PK }
+  | { kind: 'P2HMPK'; value: P2HMPK }
 
 const publicKeyCodec = new FixedSizeCodec(33)
 const keyWithIndexCodec = new ObjectCodec<KeyWithIndex>({
@@ -71,13 +76,21 @@ const p2pkCodec = new (class extends Codec<P2PK> {
   }
 })()
 
+const publicKeyLikesCodec = new ArrayCodec(publicKeyLikeCodec)
+const publicKeyIndexesCodec = new ArrayCodec(i32Codec)
+const p2hmpkCodec = new ObjectCodec<P2HMPK>({
+  publicKeys: publicKeyLikesCodec,
+  publicKeyIndexes: publicKeyIndexesCodec
+})
+
 export const unlockScriptCodec = new EnumCodec<UnlockScript>('unlock script', {
   P2PKH: publicKeyCodec,
   P2MPKH: p2mpkhCodec,
   P2SH: p2shCodec,
   SameAsPrevious: sameAsPreviousCodec,
   PoLW: publicKeyCodec,
-  P2PK: p2pkCodec
+  P2PK: p2pkCodec,
+  P2HMPK: p2hmpkCodec
 })
 
 export const encodedSameAsPrevious = unlockScriptCodec.encode({ kind: 'SameAsPrevious', value: 'SameAsPrevious' })
