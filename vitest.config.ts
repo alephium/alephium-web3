a0ac2853e38a8cb9808c841628712fa74d697e82
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-08 11:36:58 +0200

    Add missing file

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['packages/*/{src,test}/**/*.test.ts', 'test/**/*.test.ts'],
+    exclude: ['**/node_modules/**', '**/templates/**'],
+    globals: true,
+    coverage: {
+      include: ['packages/*/src/**/*.ts'],
+      exclude: ['packages/web3-react/src/**/*.ts']
+    },
+    sequence: {
+      concurrent: false
+    },
+    testTimeout: 10_000
+  }
+})
