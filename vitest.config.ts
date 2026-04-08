d47b19347d18004f31e5461fa28403ea200519a5
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-08 11:49:18 +0200

    Run tests sequentially
    The devnet tests share state (UTXOs) so they can't run in parallel.

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     include: ['packages/*/{src,test}/**/*.test.ts', 'test/**/*.test.ts'],
     exclude: ['**/node_modules/**', '**/templates/**'],
     globals: true,
+    fileParallelism: false,
     coverage: {
       include: ['packages/*/src/**/*.ts'],
       exclude: ['packages/web3-react/src/**/*.ts']
