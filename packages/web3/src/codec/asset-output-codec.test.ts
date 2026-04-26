13fce5525d1db6d1bf33f82b609c51a32a802747
h0ngcha0 <lbs.lhc@gmail.com>
2026-04-26 07:40:04 +0200

    Add asset output repro tests for P2PK and P2HMPK

---
+++ packages/web3/src/codec/asset-output-codec.test.ts
@@ -0,0 +1,59 @@
+/*
+Copyright 2018 - 2022 The Alephium Authors
+This file is part of the alephium project.
+
+The library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { addressToBytes } from '../address'
+import { AssetOutput, AssetOutputCodec } from './asset-output-codec'
+import { lockupScriptCodec } from './lockup-script-codec'
+
+describe('AssetOutputCodec', () => {
+  it('should throw for p2pk asset outputs', () => {
+    const lockupScript = lockupScriptCodec.decode(addressToBytes('3cUrKAb5KWuf61XkPorWJyNBicXG5gYTf7ZHZDKYudB4nkpD9Uu9U:3'))
+    if (lockupScript.kind !== 'P2PK') {
+      throw new Error('Expected P2PK lockup script')
+    }
+    const assetOutput: AssetOutput = {
+      amount: 1n,
+      lockupScript,
+      lockTime: 0n,
+      tokens: [],
+      additionalData: new Uint8Array([])
+    }
+
+    expect(() => AssetOutputCodec.toFixedAssetOutput(new Uint8Array(32), assetOutput, 0)).toThrow(
+      'Unexpected output script type: P2PK'
+    )
+  })
+
+  it('should throw for p2hmpk asset outputs', () => {
+    const lockupScript = lockupScriptCodec.decode(addressToBytes('AysNhoEDMej7hyoTBnVb2zNSvJ5zSj6LXoB3LwnTv2NGaZ7ufh'))
+    if (lockupScript.kind !== 'P2HMPK') {
+      throw new Error('Expected P2HMPK lockup script')
+    }
+    const assetOutput: AssetOutput = {
+      amount: 1n,
+      lockupScript,
+      lockTime: 0n,
+      tokens: [],
+      additionalData: new Uint8Array([])
+    }
+
+    expect(() => AssetOutputCodec.toFixedAssetOutput(new Uint8Array(32), assetOutput, 0)).toThrow(
+      'Unexpected output script type: P2HMPK'
+    )
+  })
+})
